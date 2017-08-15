48c7d9a84fcac5e915e295db7f56e92eb1e9dc49
Louis Orleans <louis@orleans.io>
2017-08-15 13:48:32 -0700

    🐛 There be bugs here

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,8 +8,63 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public slides: any[];
 
+  constructor(public navCtrl: NavController, fb: FormBuilder) {
+    // Define slides with inputs here
+    this.slides = [
+      {
+        title: 'Slide 1',
+        message: `This is the first slide in the deck`,
+        inputs: [
+          {
+            name: 'name',
+            label: 'Name',
+            type: 'text'
+          },
+          {
+            name: 'email',
+            label: 'Email',
+            type: 'email'
+          }
+        ],
+        form: fb.group({
+          name: new FormControl(),
+          email: new FormControl()
+        })
+      },
+      {
+        title: 'Slide 2',
+        message: `This is the second slide in the deck`,
+        inputs: [
+          {
+            name: 'age',
+            label: 'Age',
+            type: 'range'
+          },
+          {
+            name: 'gender',
+            label: 'Gender',
+            type: 'select',
+            options: [
+              {value: 'male', label: 'Male'},
+              {value: 'female', label: 'Female'},
+              {value: 'neutral', label: 'Neutral'}
+            ]
+          },
+          {
+            name: 'confirm',
+            label: 'Type Confirm',
+            type: 'text'
+          }
+        ],
+        form: fb.group({
+          age: new FormControl(),
+          gender: new FormControl(),
+          confirm: new FormControl()
+        })
+      },
+    ]
   }
 
 }
